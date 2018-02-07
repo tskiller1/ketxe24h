77419f9d5dd71cf6f3f69057c71d770ead0d0a58
hung <tskiller12345@gmail.com>
2018-02-07 14:13:56 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  return res.json({ message: "Chào mừng bạn đến với Kẹt Xe 24H lần 2" })
+  return res.json({ message: "Chào mừng bạn đến với Kẹt Xe 24H" })
 });
 
 module.exports = router;
